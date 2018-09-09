b8ff509c30b539bd1b18abf2cc5d30c78daf73b6
Chris Nunes <chrisnunes57@gmail.com>
2018-09-09 16:20:20 -0500

    Now takes input

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-from tkinter import Tk, Label, Button
+from tkinter import *
 
 class MyFirstGUI:
     def __init__(self, master):
         self.master = master
         master.title("A simple GUI")
 
+        self.frame = Frame(root)
+        self.frame.pack( side = BOTTOM )
+
         self.label = Label(master, text="Welcome to our Web Scraper!")
         self.label.pack()
 
-        self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack()
+        self.input = Entry(master)
+        self.input.pack()
 
-        self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
+        self.print_button = Button(self.frame, text="Print Input", command=self.print_input)
+        self.print_button.pack( side = LEFT)
 
-    def greet(self):
-        print("Greetings!")
+        self.close_button = Button(self.frame, text="Close", command=master.quit)
+        self.close_button.pack( side = LEFT )
+
+    def print_input(self):
+        print(self.input.get())
 
 root = Tk()
 my_gui = MyFirstGUI(root)
