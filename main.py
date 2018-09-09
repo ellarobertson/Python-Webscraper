1574bd68e6ce75631d3d21c6aacb7e20648adef1
Chris Nunes <chrisnunes57@gmail.com>
2018-09-09 16:34:28 -0500

    Formatting

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ class MyFirstGUI:
         self.master = master
         master.title("Amazon Price Webscraper")
 
+        root.geometry("700x200")
+        root.resizable(False, False)
+
         self.frame = Frame(root)
-        self.frame.pack( side = BOTTOM )
+        self.frame.pack( side = BOTTOM, pady = (0, 80) )
 
         self.label = Label(master, text="Enter the web address of an item to track")
-        self.label.pack()
+        self.label.pack(pady = (25,0))
 
         self.input = Entry(master)
         self.input.pack()
+        self.input.config(width = 80)
 
         self.print_button = Button(self.frame, text="Print Input", command=self.print_input)
-        self.print_button.pack( side = LEFT)
+        self.print_button.pack( side = LEFT, padx = (0, 10))
 
         self.close_button = Button(self.frame, text="Close", command=master.quit)
         self.close_button.pack( side = LEFT )
