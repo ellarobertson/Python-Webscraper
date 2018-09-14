e0361563d58181c5c2d4e8187e53f15deb0065d0
Chris Nunes <chrisnunes57@gmail.com>
2018-09-13 22:22:43 -0500

    Added some scraping

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from tkinter import *
+import requests
+from bs4 import BeautifulSoup
+
+
+'''Everything below here is to connect to the DB'''
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -12,11 +18,13 @@ firebase_admin.initialize_app(cred, {
   'projectId': 'price-webscraper',
 })
 
+'''Finished connecting to DB, db variable is our db reference'''
+
 db = firestore.client()
 
-class ScaperGUI:
+class ScraperGUI:
     def __init__(self, master):
-        self.print_all_links()
+
         self.master = master
         master.title("Amazon Price Webscraper")
 
@@ -33,22 +41,34 @@ class ScaperGUI:
         self.input.pack()
         self.input.config(width = 80)
 
-        self.print_button = Button(self.frame, text="Print Input", command=self.print_input)
+        self.print_button = Button(self.frame, text="Print Input", command=self.store_input)
         self.print_button.pack( side = LEFT, padx = (0, 10))
 
         self.close_button = Button(self.frame, text="Close", command=master.quit)
         self.close_button.pack( side = LEFT )
 
-    def print_input(self):
+    def store_input(self):
         user_link = self.input.get()
         db.collection(u'links').add({
             u'link': user_link
         })
+        self.get_html(user_link)
+
     def print_all_links(self):
         docs = db.collection(u'links').get()
         for doc in docs:
             print(u'{} => {}'.format(doc.id, doc.to_dict()))
 
+    def get_html(self, link):
+        try:
+            page = requests.get(link)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            price = soup.find_all(id="priceblock_ourprice")
+            print(price)
+        except:
+            print("That was not a good link! Try again.")
+
+
 root = Tk()
-my_gui = MyFirstGUI(root)
+my_gui = ScraperGUI(root)
 root.mainloop()
