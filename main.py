6a103acae5e48160b6ba26ddb27a44a98b5748d8
Chris Nunes <chrisnunes57@gmail.com>
2018-09-13 21:10:37 -0500

    Added db functionality

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from tkinter import *
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import os
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'Amazon Price Webscraper-6da255beb776.json'
+
+# Use the application default credentials
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+  'projectId': 'price-webscraper',
+})
+
+db = firestore.client()
 
 class MyFirstGUI:
     def __init__(self, master):
+        self.print_all_links()
         self.master = master
         master.title("Amazon Price Webscraper")
 
@@ -25,7 +40,14 @@ class MyFirstGUI:
         self.close_button.pack( side = LEFT )
 
     def print_input(self):
-        print(self.input.get())
+        user_link = self.input.get()
+        db.collection(u'links').add({
+            u'link': user_link
+        })
+    def print_all_links(self):
+        docs = db.collection(u'links').get()
+        for doc in docs:
+            print(u'{} => {}'.format(doc.id, doc.to_dict()))
 
 root = Tk()
 my_gui = MyFirstGUI(root)
