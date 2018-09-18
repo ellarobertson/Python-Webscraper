8d62a5c7ea482e473b9b9b26cae3b58df5235490
ellarobertson <ella.a.robertson@gmail.com>
2018-09-18 17:02:04 -0500

    set up class for sending automated email alerts

---
+++ email.py
@@ -0,0 +1,22 @@
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+
+class EmailMsg:
+
+    def __init__(self, oldPrice, newPrice, itemTitle, recipient):
+        self.un = "amazonpricetrackerproj@gmail.com"
+        self.pw = "******"
+        connectToServer()
+        sendMessage(self, recipient, oldPrice, newPrice, itemTitle)
+
+    def connectToServer(self):
+        server = smtplib.SMTP(‘smtp.gmail.com’, 587)
+        server.starttls()
+
+    def sendMessage(self, recipient, oldPrice, newPrice, itemTitle):
+        msg = MIMEMultipart()
+        msg[‘From’] = self.un
+        msg[‘To’] = recipient
+        msg[‘Subject’] = 'New Price for ' + itemTitle + '!'
+        message = Hello, /n the price for + itemTitle + has changed from + oldPrice +  to  + newPrice.
